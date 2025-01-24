1cf4310cc3c4b5569d7d92e61423bfef8c60aca9
Judah Gabriel Himango <judahgabriel@gmail.com>
2025-01-24 01:02:04 -0800

    Event listener behavior change

--- checkout.js
+++ checkout.js
@@ -56,13 +56,17 @@ class ProclaimCheckout {
       jQuery(document.body).on("woocommerce_variation_select_change", e => this.resetAddToCartBtn(e));
 
       // When we select a t-shirt design, update the t-shirt image.
-      const tshirtProductSelector = `.product-${this.tshirtId} form`;
-      const tshirtForm = document.querySelector(tshirtProductSelector);
-      if (tshirtForm) {
-         jQuery(tshirtForm).on("woocommerce_variation_select_change", e => this.showTShirtImage(e));
-      } else {
-         console.error("Couldn't find tshirt form using selector", tshirtProductSelector);
-      }
+      jquery(() => { // Inside a $(() => ...) block so that it will execute when the DOM is ready.
+         const tshirtProductSelector = `.product-${this.tshirtId} form`;
+         const tshirtForm = document.querySelector(tshirtProductSelector);
+         if (tshirtForm) {
+            jQuery(tshirtForm).on("woocommerce_variation_select_change", e => this.showTShirtImage(e));
+         } else {
+            console.error("Couldn't find tshirt form using selector", tshirtProductSelector);
+         }
+      });
+
+      document.querySelector("body").addEventListener("change", e => console.log("zanz changed fired!", e.target));
    }
 
    /**
